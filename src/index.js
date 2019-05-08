95c3b72025b403c7287792c325ca8f181a583227
DipaShaha <dipashaha92@gmail.com>
2019-05-08 14:40:51 +0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 
 
 class App extends React.Component{
 
-	constructor(props){
-		super(props);
-		this.state={lat:null, errMessage:''};
+	// constructor(props){
+	// 	super(props);
+	// 	this.state={lat:null, errMessage:''};
 
+		
+	// }
+	state={lat:null, errMessage:''};
+	componentDidMount(){
 		window.navigator.geolocation.getCurrentPosition(
-		(position)=>{ 
-			this.setState({lat:position.coords.latitude });
-		},
-		(err)=>{
-			this.setState({errMessage:err.smessage});
-		}
+			(position)=>{ 
+				this.setState({lat:position.coords.latitude });
+			},
+			(err)=>{
+				this.setState({errMessage:err.smessage});
+			}
 		);
 	}
 
@@ -28,12 +33,12 @@ class App extends React.Component{
 		}
 		if(!this.state.errMessage && this.state.lat)
 		{ 
-			return <div>latitude: {this.state.lat}</div>
+			return <SeasonDisplay lat={this.state.lat}/>
 		}
 
 		return <div>Loading...</div>
-	} //end render
-}//end class
+	}
+}
 
 
 ReactDOM.render(
